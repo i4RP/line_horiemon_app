425b6aaaaa4c0013ac28dc8a78320845d5f021ef
ダルビッシュ <shoei0205@gmail.com>
2018-05-05 23:39:27 +0900

    Improve

--- app_main.rb
+++ app_main.rb
@@ -32,6 +32,7 @@ post '/callback' do
           type: 'text',
           text: event.message['text']
         }
+          puts event.message['text']
         client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
@@ -40,6 +41,5 @@ post '/callback' do
       end
     end
   }
-  puts event.message['text']
   "OK"
 end
